6fd55ec3a151d1d4e8d9e76477ddcb1a551034ff
Victor <vitland@gmail.com>
2022-12-31 16:06:30 +0300

    refactor: Изменить вырвниване модального окна

--- blocks/popup/__close-icon/popup__close-icon.js
+++ blocks/popup/__close-icon/popup__close-icon.js
@@ -1,9 +1,8 @@
-const closeIcon = document.querySelector('.popup__close-icon')
-const popUp = document.querySelector('.popup')
+const closeIcon = document.querySelector('.popup__close-icon');
+const popUp = document.querySelector('.popup');
 
 const closePopup = () => {
-popUp.classList.remove('popup_opened')
-}
+  popUp.classList.remove('popup_opened');
+};
 
-closeIcon.addEventListener('click', closePopup)
-
+closeIcon.addEventListener('click', closePopup);
