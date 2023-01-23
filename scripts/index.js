f4fc500f5ddd7e5f8e09eb163da1970e88e88b26
Viktor Kikodze <vitland@gmail.com>
2023-01-24 00:53:48 +0300

    refactor: Добавить комменты.

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,24 @@
+//Попапы
 const popupPlace = document.querySelector(".popup_type_place");
 const popupProfile = document.querySelector(".popup_type_profile");
 const popupImage = document.querySelector(".popup_type_image");
 
-const popupCloseBtns = document.querySelectorAll(".popup__close-icon");
 const profileName = document.querySelector(".profile__name");
 const profileJob = document.querySelector(".profile__occupation");
-const placeNameInput = document.querySelector(".popup__input_type_place-name");
-const placeImageInput = document.querySelector(".popup__input_type_place-image");
-const popupForm = document.querySelectorAll(".popup__form");
 
+//Кнопки и форма попап профиля
+const popupCloseBtns = document.querySelectorAll(".popup__close-icon");
 const changeProfileBtn = document.querySelector(".profile__button-change");
-const addPlaceBtn = document.querySelector(".profile__button-add");
+const popupForm = document.querySelectorAll(".popup__form");
 const nameInput = document.querySelector(".popup__input_type_name");
 const jobInput = document.querySelector(".popup__input_type_occupation");
-const elementsContainer = document.querySelector(".elements");
 
+// Кнопка и инпуты попап новое место
+const addPlaceBtn = document.querySelector(".profile__button-add");
+const placeNameInput = document.querySelector(".popup__input_type_place-name");
+const placeImageInput = document.querySelector(".popup__input_type_place-image");
+
+const elementsContainer = document.querySelector(".elements");
 const elementsArr = [
   {
     name: "Домбай",
@@ -62,16 +66,19 @@ function closePopup() {
 }
 
 function openPopup(e) {
+  //Открытие попапа с картинкой
   if (e.target.className.includes('element__image')) {
     popupImage.classList.add("popup_opened")
     popupImage.querySelector('.popup__image').src = e.target.src
     popupImage.querySelector('.popup__image-caption').textContent = e.target.parentElement.textContent
   }
+  //Открытие попапа с профилем
   if (e.currentTarget.className.includes("button-change")) {
     popupProfile.classList.add("popup_opened");
     nameInput.value = profileName.textContent;
     jobInput.value = profileJob.textContent;
   }
+  //Открытие попап с новым местом
   if (e.currentTarget.className.includes("button-add")) {
     popupPlace.classList.add("popup_opened");
   }
@@ -80,11 +87,12 @@ function openPopup(e) {
 function submit(e) {
   e.preventDefault();
   closePopup();
+  //Сохранение профиля
   if (e.currentTarget.className.includes("type_profile")) {
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
   }
-
+  //Сохранение места
   if (
     e.currentTarget.className.includes("type_place") &&
     placeNameInput &&
@@ -108,11 +116,15 @@ function likePlace(e) {
   }
 }
 
+//Обработчик на все кнопки закрытия
 popupCloseBtns.forEach((btn) => btn.addEventListener("click", closePopup));
+
 changeProfileBtn.addEventListener("click", openPopup);
 addPlaceBtn.addEventListener("click", openPopup);
+
+//Обработчик на все кнопки сохранить
 popupForm.forEach((btn) => btn.addEventListener("submit", submit));
+
 elementsContainer.addEventListener("click", likePlace);
 elementsContainer.addEventListener("click", removePlace);
 elementsContainer.addEventListener("click", openPopup);
-
