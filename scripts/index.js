bbc7ebc9cef0bbc163e4d4ca77bfc937f012aefc
Viktor Kikodze <vitland@gmail.com>
2023-01-29 00:25:49 +0300

    fix: Вынести массив в отдельный файл, повесить слушатели кнопки закрытия через перебор.

--- scripts/index.js
+++ scripts/index.js
@@ -7,36 +7,22 @@ const profileName = document.querySelector(".profile__name");
 const profileJob = document.querySelector(".profile__occupation");
 
 //Кнопка и форма попап профиля
-const popupProfileCloseBtn = document.querySelector(".popup__close-icon_type_profile");
 const changeProfileBtn = document.querySelector(".profile__button-change");
 const profileForm = document.querySelector(".popup__form_type_profile");
 const nameInput = document.querySelector(".popup__input_type_name");
 const jobInput = document.querySelector(".popup__input_type_occupation");
 // Кнопка и инпуты попап новое место
 const addPlaceBtn = document.querySelector(".profile__button-add");
-const popupPlaceCloseBtn = document.querySelector(".popup__close-icon_type_place");
 const placeForm = document.querySelector(".popup__form_type_place");
 const placeNameInput = document.querySelector(".popup__input_type_place-name");
 const placeImageInput = document.querySelector(".popup__input_type_place-image");
 
 const elementImage = popupImage.querySelector('.popup__image')
 const elementImageCaption = popupImage.querySelector('.popup__image-caption')
-const popupImageCloseBtn = document.querySelector(".popup__close-icon_type_image");
 
+const popupCloseBtnList = document.querySelectorAll(".popup__close-icon")
 const elementsContainer = document.querySelector(".elements");
-const elementsArr = [{
-  name: "Домбай", url: "https://i.ibb.co/1m7JJCD/dombai.jpg",
-}, {
-  name: "Эльбрус", url: "https://i.ibb.co/2NmW2cp/elbrus.jpg",
-}, {
-  name: "Красноярск", url: "https://i.ibb.co/wWLHmvH/krasnoyarsk.jpg",
-}, {
-  name: "Сочи", url: "https://i.ibb.co/brGLRJ6/sochi.jpg",
-}, {
-  name: "Москва", url: "https://i.ibb.co/NxXrbCV/moscow.jpg",
-}, {
-  name: "Сочи, дендрарий", url: "https://i.ibb.co/TKL1Bnf/sochi2.jpg",
-},];
+
 const elementTemplate = document.querySelector(".elements__template").content.querySelector(".element");
 
 function populateElements(places) {
@@ -44,10 +30,11 @@ function populateElements(places) {
   elementsContainer.append(...elements);
 }
 
-function createElement({name, url}) {
+function createElement({name, link}) {
   const element = elementTemplate.cloneNode(true);
-  element.querySelector(".element__image").src = url;
-  element.querySelector(".element__image").alt = name;
+  const elementImage = element.querySelector(".element__image")
+  elementImage.src = link;
+  elementImage.alt = name;
   element.querySelector(".element__text").textContent = name;
 
   //Слушатель на удаление
@@ -57,7 +44,9 @@ function createElement({name, url}) {
   //Слушатель на лайк
   element.querySelector(".element__fav").addEventListener("click", likePlace)
   //Слушатель на открытие картинки
-  element.querySelector('.element__image').addEventListener("click", openImagePopup)
+  element.querySelector('.element__image').addEventListener("click", () => {
+    openImagePopup({name, link})
+  })
 
   return element
 }
@@ -80,10 +69,11 @@ function openPlacePopup() {
   openPopup(popupPlace)
 }
 
-function openImagePopup(evt) {
+function openImagePopup({name, link}) {
   openPopup(popupImage)
-  elementImage.src = evt.target.src
-  elementImageCaption.textContent = evt.target.parentElement.textContent
+  elementImage.src = link
+  elementImage.alt = name
+  elementImageCaption.textContent = name
 }
 
 function submitProfile(evt) {
@@ -96,10 +86,9 @@ function submitProfile(evt) {
 function submitPlace(evt) {
   evt.preventDefault();
   closePopup(popupPlace);
-  const element = createElement({name: placeNameInput.value, url: placeImageInput.value})
+  const element = createElement({name: placeNameInput.value, link: placeImageInput.value})
   elementsContainer.prepend(element)
-  placeNameInput.value = ''
-  placeImageInput.value = ''
+  placeForm.reset()
 }
 
 function removePlace(element) {
@@ -109,18 +98,12 @@ function removePlace(element) {
 function likePlace(evt) {
   evt.target.classList.toggle("element__fav_active");
 }
-
 populateElements(elementsArr)
 
 //Обработчик на все кнопки закрытия
-popupProfileCloseBtn.addEventListener("click", () => {
-  closePopup(popupProfile)
-})
-popupPlaceCloseBtn.addEventListener("click", () => {
-  closePopup(popupPlace)
-})
-popupImageCloseBtn.addEventListener("click", () => {
-  closePopup(popupImage)
+popupCloseBtnList.forEach((btn)=>{
+  const popup = btn.closest('.popup')
+  btn.addEventListener('click', ()=>{closePopup(popup)})
 })
 
 changeProfileBtn.addEventListener("click", openProfilePopup);
